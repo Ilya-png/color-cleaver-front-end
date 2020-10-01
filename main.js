51939dfdd4e747db205698d4ff1a11db383015f1
n1ckDotEXE <admin@nicholasnam.com>
2020-10-01 14:28:16 -0400

    Completed version 2.0 w/ Patrick Navarre

--- main.js
+++ main.js
@@ -4,20 +4,28 @@ const isValidPrimary = require('./is-valid-primary.js')
 const isValidSecondary = require('./is-valid-secondary.js')
 
 // Your code here!
-const color1 = process.argv[2];
-const color2 = process.argv[3];
 
-unDefined(color1, color2);
+unDefined(process.argv[2], process.argv[3]);
 
 function unDefined(primary, secondary) {
     if (primary === undefined && secondary === undefined) {
         console.log('Minimum one color required, please try again');
+
     } else if (primary !== undefined && secondary === undefined) {
-        console.log('You color ' + colorDeconstructor(primary));
+        const returnedValue1 = colorDeconstructor(primary);
+            if (returnedValue1 === undefined) {
+                console.log('Your input is not a valid color');
+            } else {
+                console.log('Your color ' + returnedValue1);
+            }
     } else if (primary !== undefined && secondary !== undefined) {
-        console.log('You color ' + colorCombinator(primary, secondary));
+        const returnedValue1 = colorCombinator(primary, secondary);
+        if (returnedValue1 === undefined) {
+            console.log('You have inputted an invalid color');
+        } else {
+            console.log(primary + ' and ' + secondary + ' combined is ' + returnedValue1);
+        }
     } else {
         console.log('NOTHING IS WORKING');
     }
 }
-
