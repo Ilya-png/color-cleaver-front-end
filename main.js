84fe74ffca09bdba2321d331d76ec38a70d16006
n1ckDotEXE <admin@nicholasnam.com>
2020-10-01 13:18:00 -0400

    Before lunch progress

--- main.js
+++ main.js
@@ -3,5 +3,21 @@ const colorDeconstructor = require('./color-deconstructor.js')
 const isValidPrimary = require('./is-valid-primary.js')
 const isValidSecondary = require('./is-valid-secondary.js')
 
-
 // Your code here!
+const color1 = process.argv[2];
+const color2 = process.argv[3];
+
+unDefined(color1, color2);
+
+function unDefined(primary, secondary) {
+    if (primary === undefined && secondary === undefined) {
+        console.log('1Minimum one color required, please try again');
+    } else if (primary !== undefined || secondary === undefined) {
+        console.log('2You color ' + colorDeconstructor(primary));
+    } else if (primary !== undefined || secondary !== undefined) {
+        console.log('3You color ' + colorCombinator(primary, secondary));
+    } else {
+        console.log('NOTHING IS WORKING');
+    }
+}
+
